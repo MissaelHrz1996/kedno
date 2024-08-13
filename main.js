f04753961f25939a75b67b217e6ae2ac7060730b
De la cruz, Missael <missael.delacruz@gov2x.com>
2024-08-12 19:36:38 -0600

    Login changed

--- main.js
+++ main.js
@@ -6,6 +6,6 @@ const button = document.querySelector(".myButton")
 document.addEventListener("DOMContentLoaded", () => {
     const button = document.querySelector(".myButton");
     button.addEventListener("click", () => {
-        console.log("D");
+        alert("Test!")
     });
 });
